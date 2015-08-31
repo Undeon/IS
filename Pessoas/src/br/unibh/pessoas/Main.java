b8c365135a98e1b3e8a3dbdce0787f298ac06517
Daniel Fonseca <undeon@gmail.com>
2015-08-31 20:51:45 -0300

    Commit aula 31/08

--- Pessoas/src/br/unibh/pessoas/Main.java
+++ Pessoas/src/br/unibh/pessoas/Main.java
@@ -1,16 +1,19 @@
 package br.unibh.pessoas;
 
+import java.util.Date;
+
+import br.unibh.pessoas.entidades.Pessoa;
+import br.unibh.pessoas.entidades.PessoaFisica;
+
 public class Main {
 	
 	
 	public static void main(String[] args) {
-		Pessoa p1 = new Pessoa();
-		p1.setNome("João");
-		Pessoa p2 = new Pessoa(1L, "Maria");
-		Pessoa p3 = new Pessoa(2L, "José", "Rua b", "3333-8888");
+
+		PessoaFisica pf1 = new PessoaFisica(1L, "João", "Rua Um, 45, Estrela Dalva", "(31) 3464-8874", "047.667.849-01", "joao@unibh.br", new Date(), "M");
 		
-		System.out.println(p1);
-		System.out.println(p2);
-		System.out.println(p3);
+		System.out.println(pf1);
+		//System.out.println(p2);
+		//System.out.println(p3);
 	}
 }
