a9044fbf99a07693d587a49baf3ac1bddcd27c2e
Daniel Fonseca <undeon@gmail.com>
2015-09-15 22:31:35 -0300

    Aula 15/09

--- Pessoas/src/br/unibh/pessoas/Main.java
+++ Pessoas/src/br/unibh/pessoas/Main.java
@@ -1,19 +1,12 @@
 package br.unibh.pessoas;
 
-import java.util.Date;
-
-import br.unibh.pessoas.entidades.Pessoa;
-import br.unibh.pessoas.entidades.PessoaFisica;
+import br.unibh.pessoas.persistencia.PessoaFisicaDAO;
 
 public class Main {
 	
 	
 	public static void main(String[] args) {
-
-		PessoaFisica pf1 = new PessoaFisica(1L, "João", "Rua Um, 45, Estrela Dalva", "(31) 3464-8874", "047.667.849-01", "joao@unibh.br", new Date(), "M");
-		
-		System.out.println(pf1);
-		//System.out.println(p2);
-		//System.out.println(p3);
+		PessoaFisicaDAO dao = new PessoaFisicaDAO();
+		dao.findAll();
 	}
 }
